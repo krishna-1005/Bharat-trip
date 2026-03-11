cf0653d0dadb7a885a58c34e79c0aeb2bf1ed098
krishna-1005 <krishkulkarni1005@gmail.com>
2026-03-11 13:36:56 +0530

    signup

--- frontend/bharat-trip/src/firebase.js
+++ frontend/bharat-trip/src/firebase.js
@@ -1,15 +1,3 @@
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDnq6rwoPL4s8GAPoZ5xqUc_jCCkS461Hs",
-//   authDomain: ,
-//   projectId: "bharat-trip-620eb",
-//   storageBucket: "bharat-trip-620eb.firebasestorage.app",
-//   messagingSenderId: "933478377830",
-//   appId: "1:933478377830:web:ee33ddcac0013a34c5c0a7",
-//   measurementId: "G-39RWP1XP34"
-// };
-
-
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
@@ -24,4 +12,5 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
+/* export auth so other files can use it */
 export const auth = getAuth(app);
