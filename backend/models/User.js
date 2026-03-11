11565f5324861c8e321c5b5d881b2054e00a2f19
krishna-1005 <krishkulkarni1005@gmail.com>
2026-03-11 16:56:58 +0530

    tey

--- backend/models/User.js
+++ backend/models/User.js
@@ -34,14 +34,13 @@ const userSchema = new mongoose.Schema(
 );
 
 /* Hash password */
-userSchema.pre("save", async function(next){
+userSchema.pre("save", async function(){
 
-  if(!this.isModified("password")) return next();
+  if(!this.isModified("password")) return;
 
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 
-  next();
 });
 
 /* Compare password */
