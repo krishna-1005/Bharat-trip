35267a028af6d163df4d6b40c963d115ed2d4b08
krishna-1005 <krishkulkarni1005@gmail.com>
2026-03-18 17:22:11 +0530

    review

--- frontend/bharat-trip/src/components/Layout.jsx
+++ frontend/bharat-trip/src/components/Layout.jsx
@@ -1,13 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 
 export default function Layout() {
+  const location = useLocation();
+  const isHomePage = location.pathname === "/" || location.pathname === "";
+
   return (
     <div className="app">
       <Navbar />
       <Outlet />
-      <Footer />
+      {isHomePage && <Footer />}
     </div>
   );
 }
