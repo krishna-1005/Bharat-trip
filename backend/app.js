e13ffffe9a516d6d7c99d02e068fa0377015a913
krishna-1005 <krishkulkarni1005@gmail.com>
2026-04-16 07:01:58 +0530

    login

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,9 @@
 const path = require("path");
-require("dotenv").config({ path: path.join(__dirname, ".env") });
+const fs = require("fs");
+const envPath = path.join(__dirname, ".env");
+if (fs.existsSync(envPath)) {
+  require("dotenv").config({ path: envPath });
+}
 const express  = require("express");
 const cors     = require("cors");
 const helmet   = require("helmet");
