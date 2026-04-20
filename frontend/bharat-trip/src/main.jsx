4b958860db82cc06b7ee218bc9f6c52d2441c0b3
krishna-1005 <krishkulkarni1005@gmail.com>
2026-04-20 11:02:18 +0530

    analytics

--- frontend/bharat-trip/src/main.jsx
+++ frontend/bharat-trip/src/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 import App from "./App";
 import "./index.css";
 import "leaflet/dist/leaflet.css";
@@ -15,6 +16,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <BrowserRouter>
           <App />
         </BrowserRouter>
+        <Analytics />
       </React.StrictMode>
     </SettingsProvider>
   </AuthProvider>
