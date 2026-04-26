b6c4fcdc46863c3bfe3541fecf52a2bd945a87d7
krishna-1005 <krishkulkarni1005@gmail.com>
2026-04-26 16:18:20 +0530

    fe

--- frontend/bharat-trip/app.config.ts
+++ frontend/bharat-trip/app.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from '@tanstack/start/config'
+import { defineConfig } from '@tanstack/react-start/config'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
+  server: {
+    preset: 'vercel',
+  },
   vite: {
     plugins: [
       tsconfigPaths(),
     ],
-  },
-  server: {
-    preset: 'vercel',
   },
 })
